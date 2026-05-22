aa775a60c896ad4500642b2e2c5a5f068817ceaa
Muhammad Fahri Ilmi <fahrilmi25@gmail.com>
2026-05-23 02:12:53 +0800

    Feat: save add acitivity data inside

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
 import sidebarView from "./js/views/sidebarView";
 
 sidebarView.setFocusMenu();
+
+const inputTitle = document.getElementById("activity-title");
+const inputCategory = document.getElementById("activity-category");
+const inputDate = document.getElementById("activity-date");
+const inputStartTime = document.getElementById("activity-start");
+const inputEndTime = document.getElementById("activity-end");
+const inputDescription = document.getElementById("activity-notes");
+const formInput = document.getElementById("new-activity-form");
+const clearInputActivity = document.querySelector(".btn-form--clear");
+
+const activityData = {};
+
+formInput.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  activityData.title = inputTitle.value;
+  activityData.category = inputCategory.value;
+  activityData.date = inputDate.value;
+  activityData.startTime = inputStartTime.value;
+  activityData.endTime = inputEndTime.value;
+  activityData.description = inputDescription.value;
+});
+
+formInput.addEventListener("reset", () => {});
